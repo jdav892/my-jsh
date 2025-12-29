65290a3008e24aa5d0b716182d060a430b3d742c
Justin Davila <jadavila9@gmail.com>
2025-12-29 11:00:04 -0500

    feat: create split lines function with constants still needing to be defined

--- jsh.c
+++ jsh.c
@@ -41,6 +41,40 @@ char *read_line()
   }
 }
 
+char **split_lines(char *line)
+{
+  int buffsize = 1024, position = 0;
+  char **tokens = malloc(buffsize * sizeof(char *));
+  char *token;
+
+  if (!tokens){
+    fprintf(stderr, "%sjsh: Allocation Error%s\n", RED, RESET);
+    exit(EXIT_FAILURE);
+  }
+  token = strtok(line, TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+
+    if (position >= buffsize)
+    {
+      buffsize += TK_BUFF_SIZE;
+      tokens = realloc(tokens, buffsize * sizeofchar(char *));
+
+      if (!tokens)
+      {
+        fprintf(stderr, "%sjsh: Allocation Error%s\n", RED, RESET);
+      }
+    }
+
+    token = strtok(NULL, TOK_DELIM);
+  }
+  tokens[position] = NULL;
+
+  return tokens;
+}
+
 void loop()
 {
   char *line;
