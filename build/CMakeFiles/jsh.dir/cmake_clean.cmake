c938381646f6a48b77097e7f97815918e15e6173
Justin Davila <jadavila9@gmail.com>
2025-12-29 13:42:56 -0500

    feat: create build for cross-platform use

---
+++ build/CMakeFiles/jsh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jsh.dir/jsh.c.o"
+  "CMakeFiles/jsh.dir/jsh.c.o.d"
+  "jsh"
+  "jsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
