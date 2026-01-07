dee597ed6a7e1add196ab390cb0f931403180603
Justin Davila <jadavila9@gmail.com>
2026-01-07 03:49:56 -0500

    feat: function to handle history command

--- builtin.c
+++ builtin.c
@@ -39,3 +39,77 @@ int jsh_exit(char **args)
   (void)args;
   return 0;
 }
+
+int jsh_history()
+{
+  FILE *fp = fopen(get_history_file_path(), "r");
+  int ch, c, line_num = 1;
+  char line[128];
+  char prev_comm[128];
+  char **args = NULL;
+  if(!fp)
+    fprintf(stderr, RED "jsh: file not found" RESET "\n");
+  else
+  {
+    putchar('\n');
+    while((c=getc(fp)) != EOF)
+    {
+      putchar(c);
+    }
+  }
+
+  printf("\n" INVERT "<0>: QUIT   <#line> Execute command <-1> clear history" RESET"\n\n: ");
+  scanf("%d", &ch);
+  getchar();
+  fseek(fp, 0, SEEK_SET);
+  if(isdigit(ch) != 0)
+  {
+    printf("Please enter a number\n");
+  }
+  else if(ch == 0)
+  {
+    fclose(fp);
+    return 1;
+  }
+  else if(ch == -1)
+  {
+    fclose(fp);
+    fp = fopen(get_history_file_path(), "w");
+    fclose(fp);
+    return jsh_execute(clr);
+  }
+  else 
+    {
+      while((fgets(line, 128, fp)) != NULL)
+      {
+        if(line_num == ch)
+        {
+          strcpy(prev_comm, &line[3]);
+          int p = 0, flag = 0;
+          fclose(fp);
+          while(prev_comm[p] != '\0')
+          {
+            if(prev_comm[p] == '|')
+            {
+              flag  = 1;
+              break;
+            }
+            p++;
+          }
+          if(!flag)
+          {
+            args = split_line(prev_comm);
+            return jsh_launch(args);
+          }
+          else
+          {
+            args = split_pipes(prev_comm);
+            return jsh_pipe(args);
+          }
+        }
+        else
+          line_num++; 
+      }
+    }
+  return 1;
+}
