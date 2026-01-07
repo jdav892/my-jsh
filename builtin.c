640aadda58b063a2289ebcfa7a82164b55c9a5b2
Justin Davila <jadavila9@gmail.com>
2026-01-07 03:46:51 -0500

    feat: create function to handle help command

--- builtin.c
+++ builtin.c
@@ -4,7 +4,7 @@ int jsh_cd(char **args)
 {
   if(args[1] == NULL)
   {
-    fprintf(stderr, "%sjsh: Please enter a path to cd%s\n", YELLOW, RESET);
+    fprintf(stderr, "%sjsh: please enter a path to cd%s\n", YELLOW, RESET);
     return 1;
   }
   else
@@ -17,3 +17,20 @@ int jsh_cd(char **args)
   }
   return 1;
 }
+
+/*
+ * Displays a description of builtin commands on - 'help'
+ * */
+
+int jsh_help(char **args)
+{
+  if(args[0] != NULL && strcmp(args[0], "help") == 0)
+  {
+    fprintf(stderr, "\n~~~~~~~\n"
+            BOLD "\njsh " RESET "is a basic unix terminal shell\n"
+            "\nSupported Commands:\n1. cd\n2. exit\n3. help\n4. touch\n5. cat"
+            "\n\n~~~~~~~\n\n");
+  }
+  return 1;
+}
+
