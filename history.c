964f9a530cbabfc99e4ed22726cf348d68016922
Justin Davila <jadavila9@gmail.com>
2026-01-07 04:01:42 -0500

    feat: create function to count amount of lines in history

--- history.c
+++ history.c
@@ -8,3 +8,19 @@ char *get_history_file_path()
   strcat(file_path, "/.jsh_history");
   return file_path;
 }
+
+int history_line_count()
+{
+  FILE *fp = fopen(get_history_file_path(), "r");
+  int c;
+  int numberOfLines = 1;
+  do
+  {
+    c = getc(fp);
+    if(c == '\n')
+    {
+      numberOfLines++;
+    }
+  }while(c != EOF);
+  return numberOfLines;
+}
