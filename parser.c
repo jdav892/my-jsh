d3b69cb7f3d5d1111156e468ecc9a5b3892b2e16
Justin Davila <jadavila9@gmail.com>
2026-01-07 04:25:13 -0500

    feat: create function to split pipes in args

--- parser.c
+++ parser.c
@@ -13,3 +13,24 @@ char *trim_whitespace(char *str)
   *(end + 1) = 0;
   return str;
 }
+
+char **split_pipes(char *input)
+{
+  char *p = strtok(input, "|");
+  char **s = malloc(1024*sizeof(char *));
+  int i = 0;
+  while(p != NULL)
+  {
+    s[i] = trim_whitespace(p);
+    i++;
+    p = strtok(NULL, "|");
+  }
+  s[i] = NULL;
+  i = 0;
+  while(s[i] != NULL)
+  {
+    printf("%s\n", s[i]);
+    i++;
+  }
+  return s;
+}
