81f67aa7f9418bbe91e2d5a588de8228dcb7dd49
Justin Davila <jadavila9@gmail.com>
2026-01-07 04:26:31 -0500

    feat: create function to split line to be read

--- parser.c
+++ parser.c
@@ -77,3 +77,38 @@ char *read_line()
     }
   }
 }
+
+char **split_line(char *line)
+{
+  int buffsize = TK_BUFF_SIZE, position = 0;
+  char **tokens = malloc(buffsize * sizeof(char *));
+  char *token;
+
+  if (!tokens){
+    fprintf(stderr, "%sjsh: Allocation Error%s\n", RED, RESET);
+    exit(EXIT_FAILURE);
+  }
+  token = strtok(line, TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+
+    if (position >= buffsize)
+    {
+      buffsize += TK_BUFF_SIZE;
+      tokens = realloc(tokens, buffsize * sizeof(char *));
+
+      if (!tokens)
+      {
+        fprintf(stderr, "%sjsh: Allocation Error%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, TOK_DELIM);
+  }
+  tokens[position] = NULL;
+
+  return tokens;
+}
