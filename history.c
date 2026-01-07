ba1509f57b2d4dd3b9e47c25c2f05ef62f627f1d
Justin Davila <jadavila9@gmail.com>
2026-01-07 04:02:40 -0500

    feat: create function to handle input for history

--- history.c
+++ history.c
@@ -24,3 +24,19 @@ int history_line_count()
   }while(c != EOF);
   return numberOfLines;
 }
+
+void history_input(char **args, char *d)
+{
+  FILE *history_file = fopen(get_history_file_path(), "a+");
+  int j = 0;
+  fprintf(history_file, "%d, ", history_line_count());
+  while(args[j] != NULL)
+  {
+    if(j > 0)
+      fputs(d, history_file);
+    fputs(args[j], history_file);
+    j++;
+  }
+  fputs("\n", history_file);
+  fclose(history_file);
+}
